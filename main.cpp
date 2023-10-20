d8fe643cbd622667ec3bc4f65b9c834b21cb80ff
tsomossi <battsolmon.a20sur11a@gmail.com>
2023-10-20 01:29:38 -0400

    user input slogan has been added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-std::cout <<"untmaarenshv/n";
+ std::cout << "untmaarenshv/n";
+ std::cout << "what is our slogan will be , dear cooomrade?/n";
+
+ std::string inputStr = "";
+ std::getline(std::cin, inputStr);
+ std::cout << inputStr , " Indeed./n";
 }
